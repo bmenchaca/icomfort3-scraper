ef0dbe5abe62b94c7f6e46be1dc69904dea31ede
Ben Menchaca <ben.menchaca@gmail.com>
2018-05-01 17:46:34 +0000

    Added the setup files, licenses, and gitignore for pypi.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'icomfort3',
+    packages = ['icomfort3'],
+    version = '0.1',
+    description = 'A library to access your Lennox S30, M30, and E30 thermostats via https://lennoxicomfort.com',
+    author = 'Ben Menchaca',
+    author_email = 'ben.menchaca@gmail.com',
+    url = 'https://github.com/bmenchaca/icomfort3',
+    download_url = 'https://github.com/bmenchaca/archive/0.1.tar.gz',
+    keywords = [ 'Lennox', 'scraper', 'thermostat' ],
+    classifiers = [],
+)
