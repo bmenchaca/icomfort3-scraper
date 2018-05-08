958f7d2993cd5aff64e73188c423d792c5c4e3f5
Ben Menchaca <ben.menchaca@gmail.com>
2018-05-08 19:32:48 +0000

    Added some retry logic to testing away mode.

--- icomfort3-scraper/test_away_mode.py
+++ icomfort3-scraper/test_away_mode.py
@@ -7,16 +7,24 @@ from lcc_zone import IComfort3Zone
 
 s = IComfort3Session()
 s.login(secrets.icomfort_username, secrets.icomfort_password)
-z = None
 for home in s.homes:
     lcc_zones = s.homes[home]
     for (lcc, zone) in lcc_zones:
         z = IComfort3Zone(home, lcc, zone)
         print ("Home %s, lcc %s, zone %s" % (home, lcc, zone))
         update = z.fetch_update(s)
+        while (update == False):
+            print("Fetch failed, retrying.")
+            s.logout()
+            s = IComfort3Session()
+            s.login(secrets.icomfort_username, secrets.icomfort_password, True)
+            update = z.fetch_update(s)
         print("Before Set Away: %s" % update['isSysteminAwayMode'])
         away = z.set_away_mode(s)
         print("After Set Away: %s" % away['isSysteminAwayMode'])
+        while (away['isSysteminAwayMode'] == 'False'):
+            away = z.set_away_mode(s)
+            print("After Set Away: %s" % away['isSysteminAwayMode'])
 
 for home in s.homes:
     lcc_zones = s.homes[home]
@@ -27,3 +35,8 @@ for home in s.homes:
         print("Before Cancel Away: %s" % update['isSysteminAwayMode'])
         cancel = z.cancel_away_mode(s)
         print("After Cancel Away: %s" % cancel['isSysteminAwayMode'])
+        while (cancel['isSysteminAwayMode'] == 'False'):
+            cancel = z.cancel_away_mode(s)
+            print("After Cancel Away: %s" % cancel['isSysteminAwayMode'])
+
+s.logout()
